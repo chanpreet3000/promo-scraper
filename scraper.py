0b6cebf2db7ff58c8296ca5aa6ab5a8f3613d844
Chanpreet Singh <chanpreet3000@gmail.com>
2024-09-12 15:13:00 +0530

    Added amazon setup, extracing coupons

--- scraper.py
+++ scraper.py
@@ -1,38 +1,19 @@
 import asyncio
-import os
-import random
 import urllib.parse
-from dotenv import load_dotenv
 from playwright.async_api import async_playwright
 
 import db
-from config import DELAY_BETWEEN_SEARCHES, DELAY_BETWEEN_PAGES, TOTAL_PAGES_TO_SCRAPE
+from config import DELAY_BETWEEN_SEARCHES, DELAY_BETWEEN_PAGES, TOTAL_PAGES_TO_SCRAPE, DELAY_BETWEEN_LINKS, POST_CODE
 from db import get_all_searches
 from logger import Logger
-from utils import sleep_randomly
-
-load_dotenv()
-
-# Load proxy credentials
-PROXY_IP = os.getenv("PROXY_IP")
-PROXY_PORT = os.getenv("PROXY_PORT")
-PROXY_USERNAME = os.getenv("PROXY_USERNAME")
-PROXY_PASSWORD = os.getenv("PROXY_PASSWORD")
-
-# User agents list
-USER_AGENTS = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-]
+from utils import sleep_randomly, get_browser
 
 
 async def scrape_search(page, search_term):
     all_product_links = []
 
-    for page_num in range(1, TOTAL_PAGES_TO_SCRAPE):
+    for page_num in range(1, TOTAL_PAGES_TO_SCRAPE + 1):
         try:
-            await sleep_randomly(DELAY_BETWEEN_PAGES)
-
             Logger.info(f"Scraping page {page_num} for '{search_term}'")
 
             encoded_search_term = urllib.parse.quote(search_term)
@@ -47,6 +28,8 @@ async def scrape_search(page, search_term):
                 "elements => elements.map(el => el.href)"
             )
             all_product_links.extend(product_links)
+
+            await sleep_randomly(DELAY_BETWEEN_PAGES)
         except Exception as e:
             Logger.error(f"Error scraping page {page_num} for '{search_term}'", e)
 
@@ -55,39 +38,9 @@ async def scrape_search(page, search_term):
 
 async def scrape_all_searches():
     async with async_playwright() as p:
-        user_data_dir = os.path.abspath("chrome_user_data")
-
-        # Ensure the directory exists
-        os.makedirs(user_data_dir, exist_ok=True)
-
-        browser = await p.chromium.launch_persistent_context(
-            user_data_dir=user_data_dir,
-            headless=False,
-            args=[
-                '--disable-blink-features=AutomationControlled',
-                '--disable-features=IsolateOrigins,site-per-process',
-                '--disable-site-isolation-trials',
-                '--disable-setuid-sandbox',
-                '--no-sandbox',
-                '--ignore-certificate-errors',
-                '--enable-features=NetworkService,NetworkServiceInProcess',
-                f'--user-agent={random.choice(USER_AGENTS)}',
-            ],
-            viewport={'width': 1920, 'height': 1080},
-            # proxy={
-            #     "server": f"http://{PROXY_IP}:{PROXY_PORT}",
-            #     "username": PROXY_USERNAME,
-            #     "password": PROXY_PASSWORD
-            # },
-            ignore_https_errors=True,
-            accept_downloads=True,
-            permissions=['geolocation'],
-        )
-
-        page = await browser.new_page()
-
-        # Array to store all product links
         all_product_links = []
+        browser = await get_browser(p)
+        page = await browser.new_page()
 
         # Loop through search terms
         search_items = await get_all_searches()
@@ -100,16 +53,88 @@ async def scrape_all_searches():
             except Exception as e:
                 Logger.error(f"Error scraping search term: {search_term}", e)
 
-        Logger.info(f"Scraping complete. Found {len(all_product_links)} product links.", all_product_links)
         await browser.close()
 
         return all_product_links
 
 
+async def scrape_product_from_links(product_links):
+    async with async_playwright() as p:
+        promo_codes = set()
+        browser = await get_browser(p)
+        page = await browser.new_page()
+        for link in product_links:
+            try:
+                await sleep_randomly(DELAY_BETWEEN_LINKS)
+                Logger.info(f"Scraping product", link)
+                await page.goto(link)
+
+                promo_element = await page.query_selector('.promoPriceBlockMessage > div')
+                if promo_element:
+                    content_id = await promo_element.get_attribute('data-csa-c-content-id')
+                    if content_id and content_id.startswith('/promo/'):
+                        promo_code = content_id.split('/promo/')[1]
+                        promo_codes.add(promo_code)
+                        Logger.info(f"Found promo code: {promo_code}")
+            except Exception as e:
+                Logger.error(f"Error scraping product {link}", e)
+
+        await browser.close()
+        return promo_codes
+
+
+async def setup_amazon_uk():
+    async with async_playwright() as p:
+        Logger.info("Setting up Amazon UK")
+        browser = await get_browser(p)
+        page = await browser.new_page()
+        # Navigate to Amazon UK
+        await page.goto('https://www.amazon.co.uk')
+
+        # Wait for and accept cookies
+        try:
+            accept_cookies_button = page.locator('#sp-cc-accept')
+            await accept_cookies_button.click(timeout=5000)
+            Logger.info("Cookies accepted")
+        except Exception as e:
+            Logger.error(f"Could not find or click cookie accept button", e)
+
+        # Set UK postcode
+        try:
+            await page.click('#glow-ingress-block')
+
+            # Wait for the postcode input field to be visible
+            postcode_input = page.locator('#GLUXZipUpdateInput')
+            await postcode_input.wait_for(state='visible', timeout=5000)
+
+            # Enter the postcode with a retry mechanism
+            await postcode_input.fill(POST_CODE)
+            await sleep_randomly(3, 0.5)
+
+            # Click the "Apply" button
+            await page.click('#GLUXZipUpdate')
+
+            # Wait for the location to update
+            await page.wait_for_selector('#glow-ingress-line2')
+
+            Logger.info("Postcode set successfully")
+            await sleep_randomly(5, 1)
+        except Exception as e:
+            raise e
+
+        Logger.info("Amazon UK setup completed")
+
+
 async def main():
     await db.connect_to_database()
+
+    await setup_amazon_uk()
+
     product_links = await scrape_all_searches()
-    Logger.info(f"Product links: {product_links}")
+    Logger.info(f"Scraping complete. Found {len(product_links)} product links.")
+
+    promo_codes = await scrape_product_from_links(product_links)
+    Logger.info(f"Found {len(promo_codes)} promo codes", promo_codes)
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,8 +1,44 @@
 import asyncio
+import os
 import random
+from config import USER_AGENTS
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 async def sleep_randomly(base_sleep: float, randomness: float = 1):
     delay = base_sleep + random.uniform(-randomness, randomness)
     delay = max(delay, 0)
     await asyncio.sleep(delay)
+
+
+async def get_browser(p):
+    user_data_dir = os.path.abspath("chrome_user_data")
+
+    # Ensure the directory exists
+    os.makedirs(user_data_dir, exist_ok=True)
+
+    # Farnham, UK coordinates (approximate)
+    latitude = 51.2150
+    longitude = -0.7986
+
+    return await p.chromium.launch_persistent_context(
+        user_data_dir=user_data_dir,
+        headless=False,
+        args=[
+            '--disable-blink-features=AutomationControlled',
+            '--disable-features=IsolateOrigins,site-per-process',
+            '--disable-site-isolation-trials',
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+            '--ignore-certificate-errors',
+            '--enable-features=NetworkService,NetworkServiceInProcess',
+            f'--user-agent={random.choice(USER_AGENTS)}',
+        ],
+        ignore_https_errors=True,
+        accept_downloads=True,
+        permissions=['geolocation'],
+        geolocation={'latitude': latitude, 'longitude': longitude},
+    )
